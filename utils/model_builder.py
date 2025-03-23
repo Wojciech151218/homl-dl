ba8bb056a9586879b1c37674c7b7c69bae24126a
Wojciech <wojciech>
2025-03-23 20:50:41 +0100

    created model_builder.py

---
+++ utils/model_builder.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+class ModelBuilder:
+    def __init__(self,data_function,model):
+        (x_train,y_train) ,(x_test,y_test) = data_function()
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.model = model
+
+    def build(self,
+              loss = "sparse_categorical_crossentropy",
+              optimizer = tf.keras.optimizers.adam,
+              learning_rate = 0.001,
+              callbacks = None,
+              metrics = None
+              ):
+        if callbacks is None:
+            callbacks = []
+        if metrics is None:
+            metrics = ['categorical_cross_entropy']
+        self.model.compile(
+            loss=loss,
+            optimizer=optimizer(learning_rate = learning_rate),
+            metrics=metrics,
+            callbacks = callbacks,
+        )
+        self.model.fit(self.x_train,self.y_train)
+        return self.model
+
+    def get_model(self):
+        return self.model
